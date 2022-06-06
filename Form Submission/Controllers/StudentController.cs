05006b36f0a1515d1c22b86aa22b4073e17ad220
Salemah <kadertanvir1@gmail.com>
2022-06-06 23:29:38 +0600

    first commit

---
+++ Form Submission/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using Form_Submission.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Form_Submission.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+       
+        [HttpGet]
+        public ActionResult Addd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Addd(Student st)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Name = st.Name;
+                return RedirectToAction("Submit");
+            }
+            return View(st);
+        }
+        
+      
+        public ActionResult Submit( FormCollection form)
+        {
+            /*
+            ViewBag.Name = Request["Name"];
+            ViewBag.Id= Request["Id"];
+            ViewBag.Dob= Request["Dob"];
+            */
+            ViewBag.Name = form["Name"];
+            ViewBag.Id = form["Id"];
+            ViewBag.Dob = form["Dob"];
+            return View();
+        }
+    }
+}
---
+++ Form Submission/Models/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Form_Submission.Models
+{
+    public class Student
+    {
+        [Required]
+        [StringLength(20, ErrorMessage = "Name lenght must not exceed 20")]
+        public string Name { get; set; }
+        [Required]
+        [RegularExpression(@"^\d{2}-\d{5}-[1-3]$", ErrorMessage = "Id must follow XX-XXXXX-X")]
+        public string Id { get; set; }
+        [Required]
+        public string Dob { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+         [Required]
+        public string Department { get; set; }
+        }
+}
